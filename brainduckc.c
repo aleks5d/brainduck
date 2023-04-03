9715e450d216d3c3c2c03d187f3b957316406b4d
Alekey Upirvitskiy <aleks5d@mail.ru>
2023-04-04 02:36:33 +0300

    simple includes

--- brainduckc.c
+++ brainduckc.c
@@ -250,16 +250,152 @@ int operation(char *name, int len, int fd) {
     return 0;
 }
 
-void clearExit(int of, int od, char *name) {
+char **processedFiles = NULL;
+int capacityPF = 0;
+int sizePF = 0;
+char **processingFiles = NULL;
+int capacityPgF = 0;
+int sizePgF = 0;
+
+int prepare_file(char *filename, int fo) {
+	for (int i = 0; i < sizePgF; ++i) {
+		if (strcmp(filename, processingFiles[i]) == 0) {
+			fprintf(stderr, "Error: Cycling include file %s\n", filename);
+			return -1;
+		}
+	}
+	for (int i = 0; i < sizePF; ++i) {
+		if (strcmp(filename, processedFiles[i]) == 0) {
+			// already processed
+			return 0;
+		}
+	}
+
+	if (sizePgF == capacityPgF) {
+		capacityPgF *= 2;
+		capacityPgF++;
+		char **newProcessingFiles = realloc(processingFiles, capacityPgF * sizeof(char *));
+		if (!newProcessingFiles) {
+			fprintf(stderr, "Error: Out of memory while processing %s\n", filename);
+			return -1;
+		}
+		processingFiles = newProcessingFiles;
+	}
+	processingFiles[sizePgF] = malloc(strlen(filename));
+	if (processingFiles[sizePgF] == NULL) {
+		fprintf(stderr, "Error: Out of memory while processing %s\n", filename);
+		return -1;
+	}
+	strcpy(processingFiles[sizePgF], filename);
+	sizePgF++;
+
+	struct stat st;
+	if (stat(filename, &st) < 0) {
+		fprintf(stderr, "Error: Cannot open file %s\n", filename);
+		return -1;
+	}
+    if (!S_ISREG(st.st_mode)) {
+        fprintf(stderr, "Error: source file %s is not regular\n", filename);
+		return -1;
+    }
+	int fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Error: Cannot open file %s\n", filename);
+		return -1;
+	}
+    char *buf = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (buf == MAP_FAILED) {
+        fprintf(stderr, "Error: Cannot create mmap on source file %s\n", filename);
+		close(fd);
+		return -1;
+    }
+
+	for (int i = 0; i < st.st_size; ++i) {
+		if (buf[i] == '#') {
+			if (i + 1 == st.st_size || buf[i + 1] != 'i'
+			 || i + 2 == st.st_size || buf[i + 2] != 'n'
+			 || i + 3 == st.st_size || buf[i + 3] != 'c'
+			 || i + 4 == st.st_size || buf[i + 4] != 'l'
+			 || i + 5 == st.st_size || buf[i + 5] != 'u'
+			 || i + 6 == st.st_size || buf[i + 6] != 'd'
+			 || i + 7 == st.st_size || buf[i + 7] != 'e'
+			 || i + 8 == st.st_size || buf[i + 8] != ' ') {
+				fprintf(stderr, "Error: Invalid usage of # character in file %s\n", filename);
+				return -1;
+			 }
+			 i += 9;
+			 while (i < st.st_size && buf[i] == ' ') ++i;
+			 if (i == st.st_size) {
+				fprintf(stderr, "Error: No filename after include in file %s\n", filename);
+				return -1;
+			 }
+			 int j = i;
+			 while (j < st.st_size && buf[j] != ' ' && buf[j] != '\n') ++j;
+			 char *newName = malloc(j - i);
+			 if (newName == NULL) {
+				fprintf(stderr, "Error: Out of memory while processing %s\n", filename);
+				return -1;
+			 }
+			 memcpy(newName, buf + i, j - i);
+			 if (prepare_file(newName, fo)) {
+				return -1;
+			 }
+			 i = j - 1;
+		} else {
+			if (tryWrite(fo, buf + i, 1)) {
+				fprintf(stderr, "Error: Error while writing to temp file in file %s\n", filename);
+				return -1;
+			}
+		}
+	}
+
+	if (capacityPF == sizePF) {
+		capacityPF *= 2;
+		capacityPF++;
+		char **newProcessedFiles = realloc(processedFiles, capacityPF * sizeof(char *));
+		if (newProcessedFiles == NULL) {
+			fprintf(stderr, "Error: Out of memory while processing %s\n", filename);
+			return -1;
+		}
+		processedFiles = newProcessedFiles;
+	}
+	processedFiles[sizePF] = malloc(strlen(processingFiles[sizePgF - 1]));
+	if (processedFiles[sizePF] == NULL) {
+		fprintf(stderr, "Error: Out of memory while processing %s\n", filename);
+		return -1;
+	}
+	strcpy(processedFiles[sizePF], processingFiles[sizePgF - 1]);
+	free(processingFiles[sizePgF - 1]);
+	--sizePgF;
+	++sizePF;
+	return 0;
+}
+
+void clearExit(int of, int od, char *name, char *tempName) {
     if (of) close(of);
     if (od) close(od);
     if (name) remove(name);
     if (name) free(name);
+	if (tempName) remove(tempName);
+	if (tempName) free(tempName);
     if (operations) {
         for (int i = 0; i < sizeOp; ++i) free(operations[i]);
         free(operations);
     }
+	for (int i = 0; i < sizePgF; ++i) free(processingFiles[i]);
+	if (processingFiles) free(processingFiles);
+	for (int i = 0; i < sizePF; ++i) free(processedFiles[i]);
+	if (processedFiles) free(processedFiles);
+}
+
+void clearExit1(int of, int od, char *name, char *tempName) {
+	clearExit(of, od, name, tempName);
     exit(1);
+}
+
+void clearExit0(int of, int od, char *name, char *tempName) {
+	clearExit(of, od, name, tempName);
+	exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -268,36 +404,44 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Error: No input files\n");
         fprintf(stderr, "Usage: brainduckc sourcefile_path [-m memsize] [-o object]\n");
-        clearExit(0, 0, 0);
-        exit(1);
+        clearExit1(0, 0, 0, 0);
     }
-    struct stat st;
-    if (stat(argv[1], &st) < 0) {
-        fprintf(stderr, "Error: Cannot open source file %s\n", argv[1]);
-        clearExit(0, 0, 0);
-    }
-    if (!S_ISREG(st.st_mode)) {
-        fprintf(stderr, "Error: source file %s is not regular\n", argv[1]);
-        clearExit(0, 0, 0);
-    }
-    int fd = open(argv[1], O_RDONLY);
+
+	char *tempName = getName();
+	int tod = open(tempName, O_WRONLY | O_CREAT | O_TRUNC, 0700);
+	if (tod < 0) {
+		fprintf(stderr, "Error: Cannot create temp file\n");
+		clearExit1(0, 0, 0, tempName);
+	}
+	if(prepare_file(argv[1], tod)) {
+		fprintf(stderr, "Error: Error while preprocessing file\n");
+		clearExit1(0, tod, 0, tempName);
+	}
+	close(tod);
+
+	struct stat st;
+	if (stat(tempName, &st) < 0) {
+		fprintf(stderr, "Error: Error cannot open temp file\n");
+		clearExit1(0, 0, 0, tempName);
+	}
+    int fd = open(tempName, O_RDONLY);
     if (fd < 0) {
-        fprintf(stderr, "Error: Cannot open source file %s\n", argv[1]);
-        clearExit(0, 0, 0);
+        fprintf(stderr, "Error: Cannot open temp file %s\n", argv[1]);
+        clearExit1(0, 0, 0, tempName);
     }
     char *buf = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (buf == MAP_FAILED) {
         fprintf(stderr, "Error: Cannot create mmap on source file %s\n", argv[1]);
-        clearExit(fd, 0, 0);
+        clearExit1(fd, 0, 0, tempName);
     }
 
 
     char* outName = getName();
     int od = open(outName, O_WRONLY | O_CREAT | O_TRUNC, 0700);
     if (od < 0) {
-        fprintf(stderr, "Error: Cannot create temp fild\n");
+        fprintf(stderr, "Error: Cannot create temp file\n");
         free(outName);
-        clearExit(fd, 0, 0);
+        clearExit1(fd, 0, 0, tempName);
     }
     {
         char *realPreamb;
@@ -315,14 +459,14 @@ int main(int argc, char *argv[]) {
         if (tryWrite(od, realPreamb, strlen(realPreamb))) {
             free(realPreamb);
             fprintf(stderr, "Error: Error while writing\n");
-            clearExit(fd, od, outName);
+            clearExit1(fd, od, outName, tempName);
         }
         free(realPreamb);
     }
 
     if (addDefaultOps()) {
         fprintf(stderr, "Error: Error while adding default operations\n");
-        clearExit(fd, od, outName);
+        clearExit1(fd, od, outName, tempName);
     }
 
     int x;
@@ -337,18 +481,18 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Error: Bad operation on line %d: ", strNum);
             printXsymbols(stderr, buf+i, j-i);
             fprintf(stderr, "\n");
-            clearExit(fd, od, outName);
+            clearExit1(fd, od, outName, tempName);
         }
         if (buf[j] == ':') {
             if (i == j) {
                 fprintf(stderr, "Error: empty function name on line %d\n", strNum);
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             }
             if (existsFunction(buf + i, j - i)) {
                 fprintf(stderr, "Error: Compilation error on line %d\n function ", strNum);
                 printXsymbols(stderr, buf+i, j-i);
                 fprintf(stderr, " created twice\n");
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             }
             if (opend) {
                 char *top;
@@ -356,7 +500,7 @@ int main(int argc, char *argv[]) {
                 if (opend == 2) top = Cpost;
                 if (tryWrite(od, top, strlen(top))) {
                     fprintf(stderr, "Error: Error while writing\n");
-                    clearExit(fd, od, outName);
+                    clearExit1(fd, od, outName, tempName);
                 }
             }
             if (j-i==4 && buf[i]=='m' && buf[i+1]=='a' && buf[i+2]=='i' && buf[i+3]=='n') {
@@ -369,21 +513,21 @@ int main(int argc, char *argv[]) {
             if (opend == 2) top = Cmain;
             if (tryWrite(od, top, strlen(top))) {
                 fprintf(stderr, "Error: Error while writing\n");
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             }
             if (opend == 1) {
                 if (tryWrite(od, buf+i, j-i)) {
                     fprintf(stderr, "Error: Error while writing\n");
-                    clearExit(fd, od, outName);
+                    clearExit1(fd, od, outName, tempName);
                 }
                 if (tryWrite(od, CFuncPrePost, strlen(CFuncPrePost))) {
                     fprintf(stderr, "Error: Error while writing\n");
-                    clearExit(fd, od, outName);
+                    clearExit1(fd, od, outName, tempName);
                 }
             }
             if (addFunction(buf + i, j - i)) {
                 fprintf(stderr, "Error: cannot add function\n");
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             }
             i = j;
             continue;
@@ -400,11 +544,11 @@ int main(int argc, char *argv[]) {
 				}
 				if (bal > 0 && j == st.st_size) {
 					fprintf(stderr, "Error: Bad bracket sequence for inline C code\n");
-					clearExit(fd, od, outName);
+					clearExit1(fd, od, outName, tempName);
 				}
 				if(tryWrite(od, buf + i + 1, j - i - 1)) {
 					fprintf(stderr, "Error: Error while writing\n");
-					clearExit(fd, od, outName);
+					clearExit1(fd, od, outName, tempName);
 				}
 				i = j;
 				continue;
@@ -413,9 +557,9 @@ int main(int argc, char *argv[]) {
                 fprintf(stderr, "Error: Bad operation on line %d: ", strNum);
                 printXsymbols(stderr, buf+i, j-i);
                 fprintf(stderr, "\nmaybe forget | after function name\n");
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             }
-        } 
+        }
         if (buf[j] == '\n') {
             strNum++;
         }
@@ -435,7 +579,7 @@ int main(int argc, char *argv[]) {
         if((RE = operation(buf + i, j - i + (buf[j] != '|'), od))) {
             if (RE == 1) {
                 fprintf(stderr, "Error: Error while writing\n");
-                clearExit(fd, od, outName);
+                clearExit1(fd, od, outName, tempName);
             } else if (RE == 2 && buf[j] == '|') {
                 fprintf(stderr, "Warning: using undeclared function on line %d: ", strNum);
                 printXsymbols(stderr, buf+i, j-i);
@@ -452,14 +596,12 @@ int main(int argc, char *argv[]) {
         if (opend == 2) top = Cpost;
         if (tryWrite(od, top, strlen(top))) {
             fprintf(stderr, "Error: Error while writing\n");
-            clearExit(fd, od, outName);
+            clearExit1(fd, od, outName, tempName);
         }
     }
 
     close(od);
     close(fd);
-    for (int i = 0; i < sizeOp; ++i) free(operations[i]);
-    free(operations);
     
     fprintf(stderr, "Ok: File created successfully\n");
     fprintf(stderr, "Ok: Starting compiling\n");
@@ -467,9 +609,7 @@ int main(int argc, char *argv[]) {
     int pid = fork();
     if (pid < 0) {
         fprintf(stderr, "Error: Cannot fork\n");
-        remove(outName);
-        free(outName);
-        exit(1);
+		clearExit1(0, 0, outName, tempName);
     }    
     if (pid == 0) {
         int id = 0;
@@ -491,8 +631,8 @@ int main(int argc, char *argv[]) {
     } else {
         fprintf(stderr, "Ok: Compile successfully\n");
     }
-    remove(outName);
-    free(outName);
+
+	clearExit0(0, 0, outName, tempName);
 
 }
 
